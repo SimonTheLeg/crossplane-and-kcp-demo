0a26937fec6d1b53ecd119c32de54882055976b6
Simon Bein <simontheleg@gmail.com>
2026-03-18 13:01:49 +0100

    add fish abbreviations for demo

---
+++ 2026-cloud-native-rejekts/demo/demo-abbr.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+# Provider
+abbr -a d1 "k get pods -n crossplane-system"
+abbr -a d2 "k neat get compositions.apiextensions.crossplane.io mysql-database-simple -o yaml | cat -l yaml"
+abbr -a d3 "cat -l yaml 3_live-demo/published-resource.yaml"
+abbr -a d4 "k apply -f 3_live-demo/published-resource.yaml"
+
+# Consumer
+abbr -a d5 "cat -l yaml 3_live-demo/apibinding.yaml"
+abbr -a d6 "k apply -f 3_live-demo/apibinding.yaml"
+abbr -a d7 "k api-resources | grep database"
+abbr -a d8 "cat -l yaml 3_live-demo/team-a-db.yaml"
+abbr -a d9 "k apply -f 3_live-demo/team-a-db.yaml"
+abbr -a d10 "k get secret"
+
+# Provider
+abbr -a d11 "k get mysqldatabases.database.mycorp.io"
+abbr -a d12 "k get secrets | grep credentials"
